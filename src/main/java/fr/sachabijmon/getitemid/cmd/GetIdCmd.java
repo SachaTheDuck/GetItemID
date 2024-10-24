51369831316b97218ad47f14069d2d4ecbf892cf
SachaTheDuck <gouron.sacha84@gmail.com>
2024-10-24 20:59:39 +0200

    Delete useless thing

--- src/main/java/fr/sachabijmon/getitemid/cmd/GetIdCmd.java
+++ src/main/java/fr/sachabijmon/getitemid/cmd/GetIdCmd.java
@@ -1,15 +1,11 @@
 package fr.sachabijmon.getitemid.cmd;
 
-import fr.sachabijmon.getitemid.GetItemId;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class GetIdCmd implements CommandExecutor {
-    private final GetItemId main;
-
-    public GetIdCmd(GetItemId main) {  this.main = main;  }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
