cc2d71be158b6710806f76dce5b701b5284b9919
SachaTheDuck <gouron.sacha84@gmail.com>
2024-10-24 20:45:48 +0200

    Initial commit

---
+++ src/main/java/fr/sachabijmon/getitemid/GetItemId.java
@@ -0,0 +1,16 @@
+package fr.sachabijmon.getitemid;
+
+import fr.sachabijmon.getitemid.cmd.GetIdCmd;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GetItemId extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        getCommand("get-id").setExecutor(new GetIdCmd(this));
+
+        getLogger().info("Plugin démarré.");
+    }
+}
---
+++ src/main/java/fr/sachabijmon/getitemid/cmd/GetIdCmd.java
@@ -0,0 +1,27 @@
+package fr.sachabijmon.getitemid.cmd;
+
+import fr.sachabijmon.getitemid.GetItemId;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GetIdCmd implements CommandExecutor {
+    private final GetItemId main;
+
+    public GetIdCmd(GetItemId main) {  this.main = main;  }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+
+        if(sender instanceof Player) {
+            Player player = (Player) sender;
+
+            main.getConfig().set("obj",player.getInventory().getItemInMainHand());
+
+            main.saveConfig();
+        }
+
+        return false;
+    }
+}
